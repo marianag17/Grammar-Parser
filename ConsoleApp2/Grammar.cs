327dcf8d43e0f40e1613dd53dcd23f7c5f466517
JoseGrn <josedanielgrn01@gmail.com>
2021-11-08 19:11:26 -0600

    Arreglos en clase grammar

--- ConsoleApp2/Grammar.cs
+++ ConsoleApp2/Grammar.cs
@@ -10,12 +10,14 @@ namespace ConsoleApp2
     {
         List<string> terminales = new List<string>();
         List<string> variables = new List<string>();
+        List<string> Reglas = new List<string>();
         string inicial;
-        public Grammar(List<string> t, List<string> v, string ini)
+        public Grammar(List<string> t, List<string> v, string ini, List<string> r)
 		{
             terminales = t;
             variables = v;
             inicial = ini;
+            Reglas = r;
 		}
         public void imprimir()
 		{
@@ -31,6 +33,17 @@ namespace ConsoleApp2
             }
             Console.WriteLine("No terminal inicial:");
             Console.WriteLine(inicial);
+
+            Console.WriteLine("Desea imprimir las reglas? (si/no)");
+            string Decision = Console.ReadLine();
+            if (Decision == "si" || Decision == "SI" || Decision == "Si" || Decision == "sI")
+            {
+                Console.WriteLine("Reglas:");
+                foreach (var item in Reglas)
+                {
+                    Console.WriteLine(item);
+                }
+            }
         }
     }
 }
--- ConsoleApp2/Parser.cs
+++ ConsoleApp2/Parser.cs
@@ -12,9 +12,10 @@ namespace ConsoleApp2
     {
         Scanner _scanner;
         Token _token;
-        string noTerminalInicial = "";
+        string noTerminalInicial = "", VariableActual = "", reglasUnidas = "";
         List<string> terminales = new List<string>();
         List<string> variables = new List<string>();
+        List<string> reglas = new List<string>();
         public string initializeGrammar(string Texto)
         {
             _scanner = new Scanner(Texto);
@@ -24,6 +25,7 @@ namespace ConsoleApp2
 
             if (_token.Tag == "Variable")
             {
+                VariableActual = _token.Value + "->";
                 noTerminalInicial = _token.Value;
                 variables.Add(_token.Value);
                 Answer = i1();
@@ -48,21 +50,32 @@ namespace ConsoleApp2
             {
                 if (_token.Tag == "Variable")
 				{
+                    reglasUnidas += _token.Value + " ";
                     if (!variables.Contains(_token.Value)) {
                         variables.Add(_token.Value);
                      }
 				}
                 else if (_token.Tag == "Terminal")
 				{
+                    reglasUnidas += _token.Value + " ";
                     if (!terminales.Contains(_token.Value))
                     {
                         terminales.Add(_token.Value);
                     }
                 }
+                else if (_token.Tag == "Pleca")
+                {
+                    reglas.Add(VariableActual+reglasUnidas);
+                    reglasUnidas = "";
+                }
                 return i2();
             }
             else if (_token.Tag == "PointComa")
+            {
+                reglas.Add(VariableActual + reglasUnidas);
+                reglasUnidas = "";
                 return i3();
+            }
             else
                 return "Error en la gramática";
         }
@@ -73,12 +86,15 @@ namespace ConsoleApp2
             if (_token.Tag == "Jump")
                 return i3();
             else if (_token.Tag == "EOF") {
-                Grammar grammar = new Grammar(terminales, variables, noTerminalInicial);
+                Grammar grammar = new Grammar(terminales, variables, noTerminalInicial, reglas);
                 grammar.imprimir();
                 return "Gramática aceptada";
             }
             else if (_token.Tag == "Variable")
+            {
+                VariableActual = _token.Value + "->";
                 return i1();
+            }
             else
                 return "Error en la gramática";
         }
